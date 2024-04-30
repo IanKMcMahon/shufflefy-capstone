305bc48343c73e95bb752742d764f337a9f6a31b
Ian McMahon <iankmcmahon@gmail.com>
2024-04-29 21:07:00 -0700

    more auth

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,9 +1,9 @@
 export const exchangeCodeForToken = async (code) => {
   const tokenUrl = "https://accounts.spotify.com/api/token";
-  const clientId = "YOUR_CLIENT_ID";
-  const clientSecret = "YOUR_CLIENT_SECRET";
-  const redirectUri = "YOUR_REDIRECT_URI";
-  const codeVerifier = "YOUR_CODE_VERIFIER";
+  const clientId = "db419786e9514488959cb7765ca0902d";
+  const clientSecret = "be28e145fe7b4f9ea08366d8a1559406";
+  const redirectUri = "localhost:3000/playlists";
+  const codeVerifier = generateRandomString(64);
 
   const requestBody = new URLSearchParams({
     grant_type: "authorization_code",
@@ -28,3 +28,26 @@ export const exchangeCodeForToken = async (code) => {
 
   return await response.json();
 };
+
+const generateRandomString = (length) => {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+};
+
+const sha256 = async (plain) => {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(plain);
+  return window.crypto.subtle.digest("SHA-256", data);
+};
+
+const base64encode = (input) => {
+  return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, "")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
+};
+
+const hashed = await sha256(codeVerifier);
+const codeChallenge = base64encode(hashed);
