18c25774b5d04d6913bd9fa5c321fcd35e1e910e
Ian McMahon <iankmcmahon@gmail.com>
2024-05-07 09:22:58 -0700

    getting a /playlist redirect

--- backend/server.js
+++ backend/server.js
@@ -15,33 +15,39 @@ app.use(bodyParser.json()); // Parse JSON bodies
 // Route for handling token exchange
 app.post("/exchange-token", async (req, res) => {
   const { code } = req.body; // Authorization code from client
-  //   const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = process.env; // Get client credentials from environment variables
   const CLIENT_ID = "db419786e9514488959cb7765ca0902d";
   const CLIENT_SECRET = "be28e145fe7b4f9ea08366d8a1559406";
   const REDIRECT_URI = "http://localhost:3000/callback";
   try {
-    const response = await axios
-      .post(
-        "https://accounts.spotify.com/api/token",
-        new URLSearchParams({
-          code: code,
-          redirect_uri: REDIRECT_URI,
-          grant_type: "authorization_code",
-        }).toString(),
-        {
-          headers: {
-            "content-type": "application/x-www-form-urlencoded",
-            Authorization:
-              "Basic " +
-              new Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString(
-                "base64"
-              ),
-          },
-        }
-      )
-      .then((res) => console.log(res.data));
-    // Send the access token and other relevant data back to the client
-    // res.json(response.data);
+    const response = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      new URLSearchParams({
+        code: code,
+        redirect_uri: REDIRECT_URI,
+        grant_type: "authorization_code",
+      }).toString(),
+      {
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          Authorization:
+            "Basic " +
+            Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64"),
+        },
+      }
+    );
+
+    // Log the entire response object for debugging
+    console.log(response);
+
+    // Check if response.data is defined before accessing it
+    if (response.data) {
+      // Log the response data for debugging
+      console.log(response.data);
+      // Send the access token and other relevant data back to the client
+      res.json(response.data);
+    } else {
+      throw new Error("Empty response from Spotify API");
+    }
   } catch (error) {
     console.error("Error exchanging code for token:", error);
     res.status(500).json({ error: "Failed to exchange code for token" });
