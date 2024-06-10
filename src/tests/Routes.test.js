8550d55e22176effd667921a8e94e6e143832b90
Your Name <you@example.com>
2024-06-10 14:26:38 -0700

    Test changes

--- src/tests/Routes.test.js
+++ src/tests/Routes.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import AppRouter from './AppRouter';
+import AppRouter from '../AppRouter';
 
 test('renders AppRouter component', () => {
   const { container } = render(
